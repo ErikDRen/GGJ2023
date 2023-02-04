20bf6109ad44f0186167f964a6d9e432b4a168b3
ErikDRen <72856412+ErikDRen@users.noreply.github.com>
2023-02-04 14:15:57 +0100

    change: menu scene

--- GGJ2023/Assets/Scripts/MenuLoadScene.cs
+++ GGJ2023/Assets/Scripts/MenuLoadScene.cs
@@ -5,6 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class MenuLoadScene : MonoBehaviour
 {
+    [SerializeField] Animator _transition;
     [SerializeField] float _transitionTime = 1f;
 
     // Start is called before the first frame update
@@ -15,6 +16,7 @@ public class MenuLoadScene : MonoBehaviour
 
     IEnumerator LoadLevel(int levelIndex)
     {
+        _transition.SetTrigger("Start");
 
         yield return new WaitForSeconds(_transitionTime);
 
