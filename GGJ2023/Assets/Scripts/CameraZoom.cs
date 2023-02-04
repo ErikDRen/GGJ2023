ceb269f8ac629f94263dbfa44eadec6a321e747a
ErikDRen <72856412+ErikDRen@users.noreply.github.com>
2023-02-04 15:07:07 +0100

    feat: camera movement

---
+++ GGJ2023/Assets/Scripts/CameraZoom.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraZoom : MonoBehaviour
+{
+    [SerializeField] Camera _cam;
+    private float camFieldOfView;
+    [SerializeField] private float zoomSpeed;
+    private float mouseScrollInput;
+    // Start is called before the first frame update
+    void Start()
+    {
+        camFieldOfView = _cam.fieldOfView;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        camFieldOfView += 10 * zoomSpeed;
+        camFieldOfView = Mathf.Clamp(camFieldOfView, 30, 80);
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            //Debug.Log("click on space for transition");
+            _cam.fieldOfView = Mathf.Lerp(_cam.fieldOfView, camFieldOfView, zoomSpeed);
+        }
+
+       
+    }
+}
