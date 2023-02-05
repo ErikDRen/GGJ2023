75827d71e9441bc45310ba30657611b4145b4432
ErikDRen <72856412+ErikDRen@users.noreply.github.com>
2023-02-05 15:52:05 +0100

    change: menu

--- GGJ2023/Assets/GameOverPanel.cs
+++ GGJ2023/Assets/GameOverPanel.cs
@@ -1,11 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameOverPanel : MonoBehaviour
 {
     public void ChangeScene(string name)
     {
         GameManager.instance.changeScene(name);
+    }
+
+    public void ReloadScene()
+    {
+        Debug.Log("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
